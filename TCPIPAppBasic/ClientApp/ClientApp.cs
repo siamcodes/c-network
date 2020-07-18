acbfd7c1f950a929956332c6c8b50844f0d3fba9
siamcodes <siamcodes@gmail.com>
2020-07-18 11:16:23 +0700

    first commit

---
+++ TCPIPAppBasic/ClientApp/ClientApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp
+{
+    class ClientApp
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+            sck.Connect(endpoint);
+            Console.Write("Enter Message: ");
+            string msg = Console.ReadLine();
+            byte[] msgBuffer = Encoding.ASCII.GetBytes(msg);
+            sck.Send(msgBuffer,0,msgBuffer.Length,0);
+
+            //รับมาจาก Server
+            byte[] buffer = new byte[1024];
+            int rec = sck.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Receive: {0}", Encoding.Default.GetString(buffer));
+            sck.Close();
+            Console.Read();
+
+        }
+    }
+}
---
+++ TCPIPAppBasic/TCPIPAppBasic/ServerApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    //Server 
+    class ServerApp
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"),9000));
+            Console.WriteLine("Wait for Connection");
+            sck.Listen(10);
+            Socket acc = sck.Accept();
+            Console.WriteLine("Connect Complete");
+            string message = "Hello World";
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            acc.Send(buffer, 0, buffer.Length, 0);
+
+            //Receive from Client
+            buffer = new byte[1024];
+            int rec = acc.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received: {0}",Encoding.Default.GetString(buffer));
+            sck.Close();
+            acc.Close();
+
+        }
+    }
+}
